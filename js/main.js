259802ed9e11cef63ef679e638cf0a139ace91c1
MishaCapral <pepefrog4you@gmail.com>
2021-11-11 11:47:01 +0200

    add modal window purchase for mobile and desktop

--- js/main.js
+++ js/main.js
@@ -254,7 +254,22 @@ $(document).ready(function () {
   });
 });
 
-
+//modal window
+//take in https://www.w3schools.com/howto/howto_css_modals.asp
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+span.onclick = function () {
+  modal.style.display = "none";
+}
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
 
